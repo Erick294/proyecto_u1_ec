972242e1a28e7453764bd910029b8925f2078538
Erick Zambrano <beowulfblackman@hotmail.com>
2022-12-08 01:59:45 -0500

    Calculadora

--- proyecto_u1_ez/js/funciones.js
+++ proyecto_u1_ez/js/funciones.js
@@ -1,16 +1,38 @@
-function sumar(num1, num2){
-    return num1 + num2
+var verificar
+
+function sumar(){
+    var suma =  parseInt(document.getElementById('primero').value) + parseInt(document.getElementById('segundo').value)
+    return suma
 }
 
-function restar(num1, num2){
-    return num1-num2
+function restar(){
+    return document.getElementById('primero').value - document.getElementById('segundo').value
 }
 
-function multiplicar(num1,num2){
-    return num1*num2
+function multiplicar(){
+    return document.getElementById('primero').value * document.getElementById('segundo').value
 }
 
-function dividir(num1,num2){
-    return num1/num2
+function dividir(){
+    return document.getElementById('primero').value / document.getElementById('segundo').value
+}
+
+function verificarPosicion1(){
+    verificar = 'primero'
+}
+
+function verificarPosicion2(){
+    verificar ='segundo'
+}
+
+function getVerificar(){
+    return verificar
+}
+
+function resetear(){
+    document.getElementById('primero').innerText='0'
+    document.getElementById('segundo').innerText='0'
+    document.getElementById('signo').innerText='-'
+    document.getElementById('resultado').innerText='0'
 }
 
